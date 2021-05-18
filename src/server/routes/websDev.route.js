b72ee345c54e80ebe0d7a2e02267f13d07878929
RiceHamburger <39089915+RiceHamburger@users.noreply.github.com>
2021-05-18 19:37:45 +0900

    関数調整

--- src/server/routes/websDev.route.js
+++ src/server/routes/websDev.route.js
@@ -5,7 +5,7 @@ import websDevCtrl from '../controllers/websDev.controller';
 const router = express.Router();
 
 router.route('/')
-.post(websDevCtrl.projectPost) /** 新增 websDev 值組 */
-.get(websDevCtrl.projectGet); /** 取得 websDev 所有值組 */
+.post(websDevCtrl.websDevPost) /** 新增 websDev 值組 */
+.get(websDevCtrl.websDevGet); /** 取得 websDev 所有值組 */
 
 export default router;
